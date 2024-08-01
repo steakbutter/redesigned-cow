03124d6c28d4d9a54b364f850c9e36f5e2ca1066
steakbutter <carlosgzzperales@gmail.com>
2024-08-01 12:18:13 -0600

    Added code to GET content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -24,7 +24,14 @@ export const putDb = async (content) => {
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
- 
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database');
 };
 initdb();
